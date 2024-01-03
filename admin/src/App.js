15ef891fc09501dcea5e5e18565d4f6174f0beac
Raulmoyano2970 <raulmoyano2970@gmail.com>
2024-01-02 21:12:24 -0300

    se termina

--- admin/src/App.js
+++ admin/src/App.js
@@ -9,8 +9,9 @@ import "./style/dark.scss";
 import { useContext } from "react";
 import { DarkModeContext } from "./context/darkModeContext";
 import { AuthContext } from "./context/authContext";
-import { hotelColumns, userColumns } from "./datatablesource";
+import { hotelColumns, userColumns, roomColumns } from "./datatablesource";
 import NewHotel from "./pages/newHotel/NewHotel";
+import NewRoom from "./pages/newRoom/NewRoom";
 
 function App() {
   const { darkMode } = useContext(DarkModeContext);
@@ -73,6 +74,24 @@ function App() {
                 element={
                 <ProtectedRoute>
                 <NewHotel />
+                </ProtectedRoute>}
+              />
+            </Route>
+            <Route path="rooms">
+              <Route index element={<ProtectedRoute><List 
+              columns={roomColumns} /></ProtectedRoute>}/>
+              <Route 
+              path=":productId" 
+              element={
+              <ProtectedRoute>
+                <Single />
+              </ProtectedRoute>} 
+              />
+              <Route
+                path="new"
+                element={
+                <ProtectedRoute>
+                <NewRoom />
                 </ProtectedRoute>}
               />
             </Route>
