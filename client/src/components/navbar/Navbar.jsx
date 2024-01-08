271da1b17a0ca3010353c8ea48b6632b3f851c81
Deroni Elgueta <deroniaeb@gmail.com>
2024-01-08 14:43:06 -0300

    Booking home

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -10,15 +10,38 @@ const Navbar = () => {
   return (
     <div className='navbar'>
         <div className='navContainer'>
+          <div className="navLogo">
             <Link to="/" style={{color: 'inherit', textDecoration: 'none'}}>
-            <span className='logo'>lamabooking</span>
+              <span className='logo'>lamabooking</span>
             </Link>
+          </div> 
+          <div className="navSection">
+            <ul className="navUl">
+              <li className="navLi">
+              Mendoza
+              </li>
+              <li className="navLi">
+                Argentina
+              </li>
+              <li className="navLi">
+                Mundo
+              </li>
+              <li className="navLi">
+                Blog
+              </li>
+              <li className="navLi">
+                Contacto
+              </li>
+            </ul>
+          </div>
+          <div className="navRegister">
             {user ? user.username : (
-              <div className='navItems'>
-                <button className='navButton'>Register</button>
-                <button className='navButton'>Login</button>
-              </div>
-            )}
+                <div className='navItems'>
+                  <button className='navButton'>Register</button>
+                  <button className='navButton'>Login</button>
+                </div>
+              )}
+          </div>
         </div>
     </div>
   )
